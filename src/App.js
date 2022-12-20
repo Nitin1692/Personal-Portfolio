31dfa1b794f9862c42def394c038a8fe2d458bb5
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-20 17:10:51 +0530

    Router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Footer from './components/footer'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
           <Footer />
+      </Router>    
     </div>
   );
 }
