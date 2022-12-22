018b852a3759bd24ee7f560bf981fbd05a0bbbdb
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-22 15:35:56 +0530

    Footer done

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,13 +3,24 @@ import { InstagramFilled, TwitterCircleFilled, FacebookFilled, LinkedinFilled } 
 import '../styles/footer.css'
 
 function footer() {
+  function instagramClick(e){
+    e.preventDefault();
+    window.location.href = "https://instagram.com/nitin_jain_16?igshid=YmMyMTA2M2Y=";
+  }
+  function twitterClick(e){
+    e.preventDefault();
+    window.location.href = "https://twitter.com/nitin_jain_16?t=MHtXdWlu5ScoswLgiiOxOw&s=35"
+  }
+  function facebookClick(e){
+    e.preventDefault();
+    window.location.href = "https://www.facebook.com/profile.php?id=100026517661870&mibextid=ZbWKwL";
+  }
   return (
         <div className="footer">
             <div className="socialMedia">
-                <InstagramFilled />
-                <TwitterCircleFilled />
-                <FacebookFilled />
-                <LinkedinFilled />
+                <InstagramFilled onClick={instagramClick} />
+                <TwitterCircleFilled onClick={twitterClick} />
+                <FacebookFilled onClick={facebookClick} />
             </div>
             <p> &copy; 2022 Nitin Jain</p>
         </div>
